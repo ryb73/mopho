686152308b6a5b71ecb4e571106aebafa12891e1
Ryan Biwer <ryan@brightcellars.com>
2017-09-28 15:17:38 -0500

    Use bs-loader

---
+++ packages/api-server/webpack.config.js
@@ -0,0 +1,66 @@
+const path              = require("path"),
+      webpack           = require("webpack");
+
+function rel(relPath) {
+    return path.resolve(__dirname, relPath)
+}
+
+module.exports = {
+    name: "api-server",
+
+    entry: {
+        index: rel("src/index.re"),
+    },
+
+    output: {
+        path: "/dev/",
+        filename: "null",
+    },
+
+    module: {
+        noParse: /node_modules/,
+        rules: [
+            {
+                test: /\.(re|ml)$/,
+                use: {
+                    loader: "bs-loader",
+                    options: {
+                        cwd: __dirname
+                    }
+                }
+            },
+        ]
+    },
+
+    resolve: {
+        extensions: ['.re', '.ml', '.js'],
+        alias: {
+            express: rel("dummy.js"),
+        }
+    },
+
+    stats: {
+        errors: true,
+        errorDetails: true,
+        warnings: true,
+        colors: true,
+        timings: true,
+
+        assets: false,
+        cached: false,
+        cachedAssets: false,
+        children: false,
+        chunks: false,
+        depth: false,
+        entrypoints: false,
+        hash: false,
+        modules: false,
+        performance: false,
+        providedExports: false,
+        publicPath: false,
+        reasons: false,
+        source: false,
+        usedExports: false,
+        version: false,
+    }
+};
